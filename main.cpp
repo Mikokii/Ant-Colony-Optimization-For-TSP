93437b7863d7321d298c4cea48465b40d524021e
Kasia Jaromirska <kasia.jaromirska@gmail.com>
2023-11-22 12:30:44 +0100

    Added passing filenames in arguments

--- main.cpp
+++ main.cpp
@@ -1,29 +1,42 @@
 #include <iostream>
+#include <string>
 #include "Graph.hpp"
 
 Graph graph;
 
-int main(){
+int main(int argc, char *argv[]){
 
     int answer=-1;
+    std::string filename;
 
-    std::cout << "What input do you want to use?" << std::endl;
-    std::cout << "File (0) or Your input (1)" << std::endl;
-    std::cin >> answer;
+    if(argc < 1){
+        std::cout << "What input do you want to use?" << std::endl;
+        std::cout << "File (0) or Your input (1)" << std::endl;
+        std::cin >> answer;
 
-    if(answer){
-        int vert, range;
-        std::cout << "Vertex number: ";
-        std::cin >> vert;
-        std::cout << "Range: ";
-        std::cin >> range;
-        
-       graph.initRandomGraph(vert, range);
-       graph.saveGraphToFile("out.txt");
+        if(answer){
+            int vert, range;
+            std::cout << "Vertex number: ";
+            std::cin >> vert;
+            std::cout << "Range: ";
+            std::cin >> range;
+            
+            graph.initRandomGraph(vert, range);
+
+            graph.saveGraphToFile("out.txt");
+        }
+            else graph.initGraphFromFile("test.txt");
+            filename = "test";
     }
-    else{
-        graph.initGraphFromFile("test.txt");
+    else {
+        graph.initGraphFromFile(argv[1]);
+        std::string filename(argv[1]);
+        filename =  filename.substr(0, filename.find("."));
+        std::cout << filename << std::endl;
     }
+
+    filename += "_wynik.txt";
+
     graph.generateAdjacencyMatrix();
     // graph.printGraph();
 
@@ -31,6 +44,7 @@ int main(){
     float min_distance = result.first;
     std::vector<int> min_route = result.second;
 
+    //std::ofstream outFile; - trzeba tu zrobić wpisywanie wyników do filename pliku, nie dziala mi deklaracje idk why
     std::cout << "Calculated distance: " << min_distance << std::endl;
     std::cout << "Calculated route: ";
     for (int i = 0; i < min_route.size(); i++){
